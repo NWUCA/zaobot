4e49491f5c5e438473aa30daeea16ec82a8041df
cjc7373 <niuchangcun@163.com>
2019-11-21 18:16:43 +0800

    try to refactor code

--- coolq.py
+++ coolq.py
@@ -1,9 +1,9 @@
-from aiocqhttp import CQHttp
+from cqhttp import CQHttp
 import re
 from datetime import datetime, time, date, timedelta
 import json
 
-bot = CQHttp(enable_http_post=False)
+bot = CQHttp(api_root='http://127.0.0.1:5700/')
 
 today_date = date.today()
 repeat_mode = 0
@@ -21,11 +21,18 @@ except IOError:
 def reply(msg):
     return {'reply': msg}
 
+
+def remove_timeout_user(user_id):
+    now = datetime.now()
+    waken_time = datetime.fromtimestamp(waken_list[user_id]['time'])
+    duration = now - waken_time
+    if duration > timedelta(hours=12):
+        del waken_list[user_id]
+
+    
 @bot.on_message()
-async def handle_msg(context):
-    global today_date
-    global waken_num
-    global repeat_mode
+def handle_msg(context):
+    global today_date, waken_num, repeat_mode
     if re.match('/', context['message']) is not None:
         message = context['message'][1:]
         message = message.split()
@@ -41,12 +48,8 @@ async def handle_msg(context):
             return {'reply': '我的源码存放在：github.com/cjc7373/zaobot，尽情探索吧。'}
 
         elif command == 'zao':
-            if waken_list.get(context['user_id']):
-                now = datetime.now()
-                waken_time = datetime.fromtimestamp(waken_list[context['user_id']]['time'])
-                duration = now - waken_time
-                if duration > timedelta(hours=12):
-                    del waken_list[context['user_id']]
+            remove_timeout_user(context['user_id'])
+
             if waken_list.get(context['user_id']) is None:
                 waken_list[context['user_id']] = {'time': context['time']}
                 if context['sender'].get('card') is None:
@@ -55,7 +58,7 @@ async def handle_msg(context):
                     waken_list[context['user_id']]['nickname'] = context['sender']['card']
                 waken_num += 1
                 if waken_num == 1:
-                    await bot.send(context, "获得成就：早起冠军")
+                    bot.send(context, "获得成就：早起冠军")
                 try:
                     greeting = args[0]
                 except IndexError:
@@ -80,15 +83,12 @@ async def handle_msg(context):
 
         elif command == 'zaoguys':
             waken_list_sorted = sorted(waken_list.items(), key=lambda value: value[1]['time'])
-            now = datetime.now()
             msg = ""
             index = 1
             for person in waken_list_sorted:
+                remove_timeout_user(person[1])
                 waken_time = datetime.fromtimestamp(person[1]['time'])
                 waken_date = date.fromtimestamp(person[1]['time'])
-                duration = now - waken_time
-                if duration > timedelta(hours=12):
-                    del waken_list[person[0]]
                 if waken_date == today_date:
                     msg += f"\n{index}. {person[1]['nickname']}, {waken_time.hour:02d}:{waken_time.minute:02d}"
                     index += 1
@@ -97,41 +97,58 @@ async def handle_msg(context):
             return {'reply': msg}
 
         elif command == 'flush':
-            if context['sender'].get('role') == 'owner' or context['sender'].get('role') == 'admin'\
-                    and context.get('group_id') == 102334415:
-                waken_list.clear()
-                waken_num = 0
-                repeat_mode = 0
-                today_date = date.today()
-                return {'reply': "咱也不知道发生了什么，咱也不敢问。"}
-            else:
-                return {'reply': "你没有权限o(≧口≦)o"}
+            flush_handler(context)
+        
+        elif command == 'fudu':
+            fudu_handler(context)
 
-        elif command == 'fudu' \
-                and context.get('group_id') == 102334415 \
-                and (context['sender'].get('role') == 'owner' or context['sender'].get('role') ==  'admin'):
-            if repeat_mode == 0:
-                repeat_mode = 1
-                return {'reply': "复读模式已开启(๑•̀ㅂ•́)و✧"}
-            else:
-                repeat_mode = 0
-                return {'reply': "复读模式已关闭～(　TロT)σ"}
+        elif command == 'save':
+            save_handler(context)
 
-        elif command == 'save' \
-                and context.get('group_id') == 102334415 \
-                and (context['sender'].get('role') == 'owner' or context['sender'].get('role') == 'admin'):
-            try:
-                with open('waken_list.json', 'w') as f:
-                    json.dump(waken_list, f)
-                with open('waken_num.json', 'w') as f:
-                    json.dump(waken_num, f)
-                return reply("持久化数据成功。")
-            except IOError as e:
-                return reply(e)
         else:
             return {'reply': '听不懂<(=－︿－=)>'}
 
     elif repeat_mode:
         return {'reply': context['message'], 'at_sender': False}
+
+class admin_required():
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        if args[0].get('group_id') == 102334415 \
+            and (args[0]['sender'].get('role') == 'owner' or args[0]['sender'].get('role') == 'admin'):
+            return self.func(*args, **kwargs)
+        else:
+            return reply("你没有权限o(≧口≦)o")
+
+@admin_required
+def fudu_handler(context):
+    if repeat_mode == 0:
+        repeat_mode = 1
+        return {'reply': "复读模式已开启(๑•̀ㅂ•́)و✧"}
+    else:
+        repeat_mode = 0
+        return {'reply': "复读模式已关闭～(　TロT)σ"}
+
+@admin_required
+def save_handler(context):
+    try:
+        with open('waken_list.json', 'w') as f:
+            json.dump(waken_list, f)
+        with open('waken_num.json', 'w') as f:
+            json.dump(waken_num, f)
+        return reply("持久化数据成功。")
+    except IOError as e:
+        return reply(e)
+
+@admin_required
+def flush_handler(context):
+    global today_date, waken_num, repeat_mode
+    waken_list.clear()
+    waken_num = 0
+    repeat_mode = 0
+    today_date = date.today()
+    return {'reply': "清除数据成功。"}
 
 bot.run(host='0.0.0.0', port=8080)
