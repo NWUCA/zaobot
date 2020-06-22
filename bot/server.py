8b9fb49dc3efa37600ed89611f994da8b3aee275
cjc7373 <niuchangcun@gmail.com>
2020-06-22 15:05:48 +0800

    fix: minor changes
    修复了随机存放至树洞

--- bot/server.py
+++ bot/server.py
@@ -33,16 +33,6 @@ def handler():
     payload = request.json
     post_type = payload.get("post_type")
 
-    # type_key = payload.get(
-    #     {'message': 'message_type',
-    #      'notice': 'notice_type',
-    #      'event': 'event',  # compatible with v3.x
-    #      'request': 'request_type',
-    #      'meta_event': 'meta_event_type'}.get(post_type)
-    # )
-    # if not type_key:
-    #     abort(400)
-
     if post_type != "message":
         abort(400)
 
@@ -77,6 +67,7 @@ def handler():
 def pre_process(context: Context):
     utils.log(context)
     utils.accumulate_exp(context)
+    utils.randomly_save_message_to_treehole(context)
 
     if context.message_type == 'group':
         utils.find_cai(context)
