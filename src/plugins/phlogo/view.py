90f35bf4796f018b72a2a7742f921fb8c8733518
张钧博 <810505685@qq.com>
2021-05-22 20:37:48 +0800

    .vscode

---
+++ src/plugins/phlogo/view.py
@@ -0,0 +1,29 @@
+import asyncio
+from pathlib import  Path
+
+from nonebot import on_shell_command
+from nonebot.rule import ArgumentParser
+from nonebot.typing import T_State
+from nonebot.adapters import Bot, Event
+
+PARENT_PATH = Path(__file__).parent
+IMAGE_PATH = (PARENT_PATH / 'ph.png').resolve()
+
+ph_parser = ArgumentParser()
+ph_parser.add_argument('left')
+ph_parser.add_argument('right')
+
+ph = on_shell_command('ph', parser=ph_parser)
+@ph.handle()
+async def _(bot: Bot, event: Event, state: T_State):
+    left = getattr(state['args'], 'left', None)
+    right = getattr(state['args'], 'right', None)
+    if not left and right:
+        return
+    await asyncio.create_subprocess_shell(
+        f'python {PARENT_PATH}/ph_logo.py {left} {right}'
+    )
+    await ph.finish([
+        {"type": "image", "data": {"file": f'file:///{IMAGE_PATH}'}},
+    ])
+    
