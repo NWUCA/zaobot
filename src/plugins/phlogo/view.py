1698ccecbbe1038f8121ece1f518be4800f11325
张钧博 <34473927+MIXISAMA@users.noreply.github.com>
2021-05-23 20:41:19 +0800

    加锁

--- src/plugins/phlogo/view.py
+++ src/plugins/phlogo/view.py
@@ -10,6 +10,7 @@ from .util import safe
 
 PARENT_PATH = Path(__file__).parent
 IMAGE_PATH = (PARENT_PATH / 'ph.png').resolve()
+lock = asyncio.Lock()
 
 ph_parser = ArgumentParser()
 ph_parser.add_argument('left')
@@ -22,9 +23,14 @@ async def _(bot: Bot, event: Event, state: T_State):
     right = safe(getattr(state['args'], 'right', None))
     if not left or not right:
         return
-    cmd = f'python {PARENT_PATH}/ph_logo.py {left} {right}'
-    process = await asyncio.create_subprocess_shell(cmd)
-    await process.wait()
-    await ph.finish([
-        {"type": "image", "data": {"file": f'file:///{IMAGE_PATH}'}},
-    ])
+
+    async with lock:
+        try:
+            cmd = f'python {PARENT_PATH}/ph_logo.py {left} {right}'
+            process = await asyncio.create_subprocess_shell(cmd)
+        except:
+            return
+        await process.wait()
+        await ph.finish([
+            {"type": "image", "data": {"file": f'file:///{IMAGE_PATH}'}},
+        ])
