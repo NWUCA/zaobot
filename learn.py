bacd7dce27e02fd2f63d6fe8bc53c3e50d9fcecf
张钧博 <810505685@qq.com>
2022-11-19 00:34:28 +0800

    prepare for nb2

--- learn.py
+++
@@ -1,8 +0,0 @@
-dic = {
-    'cc': {'time': '123133', 'nickname': ''},
-    'aa': {'time': '123123', 'nickname': ''},
-    'bb': {'time': '123124', 'nickname': ''}
-}
-print(sorted(dic.items(), key=lambda value: value[1]['time']))
-for i in dic.values():
-    print(i)
