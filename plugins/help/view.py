b94f16514ef47c1f974028253b5d25cddb9e1a1c
张钧博 <810505685@qq.com>
2022-11-20 00:07:27 +0800

    仓库迁移

--- plugins/help/view.py
+++ plugins/help/view.py
@@ -3,4 +3,4 @@ from nonebot import on_command
 help = on_command('help', aliases={'帮助', '?', 'git', 'github'}, priority=2, block=True)
 @help.handle()
 async def _():
-    await help.finish('https://github.com/MIXISAMA/zaobot', at_sender=True)
+    await help.finish('https://github.com/NWUCA/zaobot/', at_sender=True)
