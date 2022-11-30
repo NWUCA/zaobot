c158559072947c6f15b347f825def2b2e3d755b1
张钧博 <810505685@qq.com>
2022-12-01 00:52:06 +0800

    天气文本增强为天气卡片图

--- plugins/weather/task.py
+++ plugins/weather/task.py
@@ -1,13 +1,30 @@
-from datetime import datetime
 import nonebot
+from nonebot.adapters.onebot.v11 import MessageSegment
 
 from database.schedule.decorator import task_register
 from database.group.method import get_group_location
-from .data_source import get_grid_weather_today
+
+from .data_source import get_grid_weather_today, gen_weather_card_image
+
+@task_register(key='推送今日网格天气')
+async def _(group_id: str):
+    await send_image_weather_card(group_id)
+
+async def send_image_weather_card(group_id: str):
+    location = await get_group_location(group_id)
+    if location is None or location.east_longitude is None or location.north_latitude is None:
+        print('请在后台设置经纬度坐标')
+        return
+    img = await gen_weather_card_image(location.east_longitude, location.north_latitude, location.brief)
+    if not img:
+        print('生成天气卡片失败')
+        return
+    bot = nonebot.get_bot()
+    await bot.send_group_msg(group_id=group_id, message=MessageSegment.image(img))
+
 
 async def send_message_grid_weather_today(group_id: str):
-    print(datetime.now(), '推送今日网格天气', group_id)
-
+    # 弃用
     location = await get_group_location(group_id)
     if location is None or location.east_longitude is None or location.north_latitude is None:
         print('请在后台设置经纬度坐标')
@@ -31,7 +48,3 @@ async def send_message_grid_weather_today(group_id: str):
 
     bot = nonebot.get_bot()
     await bot.send_group_msg(group_id=group_id, message=msg)
-
-@task_register(key='推送今日网格天气')
-async def _(group_id: str):
-    await send_message_grid_weather_today(group_id)
